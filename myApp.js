c95675290d01958ccac6ef4561bfdfdade24cfbe
Glitch (archaengel-boilerplate-project-exercisetracker) <none>
2019-03-03 00:59:18 +0000

    🎳🔦 Updated with Glitch - Update npm packages and add connection to MongoDB

---
+++ myApp.js
@@ -0,0 +1,10 @@
+const mongodb = require('mongodb');
+const mongoose = require('mongoose');
+
+mongoose.connect(
+  process.env.MONGO_URI,
+  {
+    useNewUrlParse: true,
+    dbname: 'exercisetracker'
+  }
+);
